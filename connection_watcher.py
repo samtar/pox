117427afc46b95b8c9d369085f9b2f01fd442873
samtar <samtar@users.noreply.github.com>
2013-12-07 20:44:39 +0530

    Create connection_watcher.py

---
+++ connection_watcher.py
@@ -0,0 +1,18 @@
+"""
+All connected switches (DPID) will be appear.
+"""
+
+from pox.core import core
+from pox.lib.util import dpid_to_str
+
+log = core.getLogger()
+
+class MyComponent (object):
+  def __init__ (self):
+    core.openflow.addListeners(self)
+
+  def _handle_ConnectionUp (self, event):
+    log.debug("Switch %s has come up.", dpid_to_str(event.dpid))
+
+def launch ():
+  core.registerNew(MyComponent)
